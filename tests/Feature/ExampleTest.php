6d56933ffffa23b9aa69387401d06567c89b1a20
Emil Lau <emil.lau@xterraplanet.com>
2026-06-27 23:17:12 +0800

    Current file just in case

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    use LazilyRefreshDatabase;
+
+    #[Test]
+    public function can_create_note()
+    {
+        
+    }
+}
